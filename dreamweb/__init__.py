bfbd1cdc72486b7d7febd0f3548bd44a92613471
Grandpa EJ <abirmridha2005@gmail.com>
2025-11-22 23:41:37 +0600

    Bump version to 0.1.1

--- dreamweb/__init__.py
+++ dreamweb/__init__.py
@@ -11,5 +11,5 @@ Usage:
 
 from dreamweb.core import App
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __all__ = ["App"]
--- test_serialization.py
+++
@@ -1,8 +0,0 @@
-"""Test the serialization"""
-from examples.hello_world import HelloWorldApp
-
-app = HelloWorldApp()
-tree = app._widget_to_dict(app.build())
-
-import json
-print(json.dumps(tree, indent=2))
